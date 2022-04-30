87af7df4221ae4e57e16ca7bcde0d5a9682af632
witktom5 <witktom5@gmail.com>
2022-04-30 12:23:08 +0200

    set up error handler, catch async

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
+const { errorHandler } = require('./middleware/errorHandler');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Support Desk API');
@@ -11,5 +15,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,24 @@
-module.exports.registerUser = (req, res) => {
-  res.send('Register route');
-};
+const catchAsync = require('../utils/catchAsync');
 
-module.exports.loginUser = (req, res) => {
+// Register a new user
+// /api/users
+// Public
+
+module.exports.registerUser = catchAsync(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill all fields');
+  }
+
+  res.send('Register route');
+});
+
+// Login a user
+// /api/users/login
+// Public
+
+module.exports.loginUser = catchAsync(async (req, res) => {
   res.send('Login route');
-};
+});
